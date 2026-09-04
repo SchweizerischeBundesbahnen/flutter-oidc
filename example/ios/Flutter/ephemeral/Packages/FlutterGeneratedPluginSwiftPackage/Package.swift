acc313f024c0d437d97252dd835968d748b122ef
Ulrich Raab <14219301+UlrichRaab@users.noreply.github.com>
2026-09-04 09:50:19 +0200

    Update Dart and Flutter

--- example/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
+++ example/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
-//  Generated file. Do not edit.
+// Generated file. Do not edit.
 //
 
 import PackageDescription
@@ -9,17 +9,24 @@ import PackageDescription
 let package = Package(
     name: "FlutterGeneratedPluginSwiftPackage",
     platforms: [
-        .iOS("13.0")
+        .iOS("15.0")
     ],
     products: [
         .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
     ],
     dependencies: [
-        
+        .package(name: "flutter_appauth", path: "../.packages/flutter_appauth-12.0.0"),
+        .package(name: "flutter_secure_storage_darwin", path: "../.packages/flutter_secure_storage_darwin-0.2.0"),
+        .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
     ],
     targets: [
         .target(
-            name: "FlutterGeneratedPluginSwiftPackage"
+            name: "FlutterGeneratedPluginSwiftPackage",
+            dependencies: [
+                .product(name: "flutter-appauth", package: "flutter_appauth"),
+                .product(name: "flutter-secure-storage-darwin", package: "flutter_secure_storage_darwin"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ]
         )
     ]
 )
